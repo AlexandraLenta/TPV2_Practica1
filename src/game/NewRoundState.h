fe58d1db17076d058afa837ab617b3788b08eacd
Zhiyi Zhou <zhiyiz01@ucm.es>
2026-03-04 23:59:34 +0100

    Crear algunas clases

---
+++ src/game/NewRoundState.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "GameState.h"
+
+class NewRoundState :
+    public GameState
+{
+public:
+    NewRoundState() {}
+    virtual ~NewRoundState() {}
+
+    void enter() override;
+    void leave() override;
+    void update() override;
+};
+
