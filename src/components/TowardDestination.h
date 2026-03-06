8bbe2089008937396826bff3994f35a125e99534
Zhiyi Zhou <zhiyiz01@ucm.es>
2026-03-06 02:45:23 +0100

    cosas sin subir

--- src/components/TowardDestination.h
+++ src/components/TowardDestination.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../ecs/Component.h"
+
 class TowardDestination : public ecs::Component
 {
 };
--- src/game/AsteroidsUtils.h
+++ src/game/AsteroidsUtils.h
@@ -10,7 +10,7 @@ public:
 
     void create_asteroids(int n) override;
     void remove_all_asteroids() override;
-    void split_asteroid(ecs::Entity a) override;
+    void split_asteroid(ecs::Entity* a) override;
 
 private:
     ecs::EntityManager* _mngr;
