32a522b5ea0592fd026a167a015e4e7c721f9d5a
Zhiyi Zhou <zhiyiz01@ucm.es>
2026-03-07 02:42:21 +0100

    Quitar cambio ilegal

--- src/components/ImageWithFrames.h
+++ src/components/ImageWithFrames.h
@@ -20,11 +20,10 @@ public:
 
     void render() override;
     void update() override;
-    const Texture* _tex;
 
 private:
     int _rows, _cols, _frame, _lastChangeTime = 0;
-
+    const Texture* _tex;
     Transform* _tr;
     std::vector<SDL_FRect> _srcRects;
 
