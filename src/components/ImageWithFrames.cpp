015521120e9198c1518511f82eff3a8c2336035b
Alexandra <alexlent@ucm.es>
2026-03-07 16:15:53 +0100

    FIXED

--- src/components/ImageWithFrames.cpp
+++ src/components/ImageWithFrames.cpp
@@ -10,7 +10,7 @@ ImageWithFrames::ImageWithFrames(const Texture* tex, int rows, int cols) : _tex(
 
 	for (int i = 0; i < _rows; i++) {
 		for (int j = 0; j < _cols; j++) {
-			_srcRects.push_back(SDL_FRect{i * _frameW, j* _frameH, _frameW, _frameH });
+			_srcRects.push_back(SDL_FRect{j * _frameW, i * _frameH, _frameW, _frameH });
 		}
 	}
 }
@@ -35,13 +35,8 @@ ImageWithFrames::update() {
 
 void
 ImageWithFrames::render() {
-	int row = _frame / _cols;
-	int col = _frame % _cols;
-	SDL_FRect src = build_sdlfrect((float)(col * _frameW), (float)(row * _frameH), _frameW, _frameH);
-
 	// dest rect for the current frame
 	SDL_FRect dest = build_sdlfrect(_tr->getPos(), _tr->getWidth(), _tr->getHeight());
 	// render the src frame rect to the dest rect
-	//_tex->render(_srcRects[_frame], dest);
-	_tex->render(src, dest);
+	_tex->render(_srcRects[_frame], dest);
 }
